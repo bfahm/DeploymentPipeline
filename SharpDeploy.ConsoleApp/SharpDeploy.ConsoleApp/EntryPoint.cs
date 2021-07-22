6d9fc5c213e28c0ab4b1a7b03f77c53b6df0d421
Paula Fahmy <paula.fahmy@flairstech.com>
2021-07-22 06:48:40 +0200

    Added a Console App endpoint for flexible usage of core logic.

---
+++ SharpDeploy.ConsoleApp/SharpDeploy.ConsoleApp/EntryPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SharpDeploy.ConsoleApp
+{
+    class EntryPoint
+    {
+        private readonly Deployer _deployer;
+
+        public EntryPoint(Deployer deployer)
+        {
+            _deployer = deployer;
+        }
+
+        private void Index()
+        {
+            var listOfApps = _deployer.GetListOfApplications();
+
+            Console.WriteLine("Available Projects:");
+            Console.WriteLine("");
+
+
+            foreach (var app in listOfApps)
+            {
+                Console.WriteLine($"ID: {app.Id}");
+                Console.WriteLine($"Name: {app.Name}");
+                Console.WriteLine($"Title: {app.Title}");
+                
+                Console.WriteLine("");
+            }
+        }
+
+        public async Task<string> Deploy()
+        {
+            Console.WriteLine("----- SHARP DEPLOYER -----");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Index();
+
+            Console.WriteLine("Enter the following required data:");
+            Console.Write("Project Id:\t\t");
+            var projectId = Console.ReadLine();
+
+            Console.Write("Git Email:\t\t");
+            var gitEmail = Console.ReadLine();
+            Console.Write("Git Personal Token:\t");
+            var gitToken = Console.ReadLine();
+            
+
+            return await _deployer.DeployDotNet(projectId, gitEmail, gitToken);
+        }
+    }
+}
---
+++ SharpDeploy.ConsoleApp/SharpDeploy.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharpDeploy.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace SharpDeploy.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+            await Run(serviceProvider);
+
+        }
+
+        static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<EntryPoint>();
+            services.AddCore();
+                
+            return services.BuildServiceProvider();
+        }
+
+        static async Task Run(IServiceProvider serviceProvider)
+        {
+            var entryPoint = serviceProvider.GetService<EntryPoint>();
+            var result = await entryPoint.Deploy();
+            Console.WriteLine(result);
+        }
+    }
+}
